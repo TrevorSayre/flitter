b577e5972ae440f46370b5fc59d0164c769d291f
sayret <sayret@4cf41a06-4af6-11de-8001-7f2440edb24e>
2009-05-29 23:50:26 +0000

    Cleanup

--- trunk/index.php
+++ trunk/index.php
@@ -3,10 +3,9 @@
 <html>
 <head>
 <title>Index Page</title>
-<script src="js/testing.js" type="text/javascript"></script>
 </head>
 <body>
 This page is the public Index Page.<br />
-<?php echo 'Try to go to the <a href="secret.php">Secret Page</a>.'; ?>
+Try to go to the <a href="secret.php">Secret Page</a>
 </body>
 </html>
