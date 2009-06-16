ebb147db4d792b0d550ab1fb70b679afbc32c865
graylin.kim <graylin.kim@4cf41a06-4af6-11de-8001-7f2440edb24e>
2009-06-16 17:35:51 +0000

    Incorporated a neat little templating system based of a templater class and capturing eval output in output buffers.

--- templates.php
+++ templates.php
@@ -1,62 +1,103 @@
 <?php
 
-//File for our template system
-function load_template( $template_name, $template = array(), $show_warnings = true ) {
+//This function checks a template file for errors
+//Includes it if everything checks out
+//Takes template_name -> string with the relative path of the template file
+//      template -> an array (name => value) of variables for template usage
+//      show_warnings if they want
+
+class Templater {
+  public  $dump_warnings;
+  private $warnings;
+  private $scripts;
+  private $styles;
   
-  $warnings = array();
-  $vars = array();
+  function __construct() {
+    $this->dump_warnings = true;
+    $this->warnings = array();
+    $this->scripts = array();
+    $this->styles = array();
+  }
   
-  if(file_exists($template_name))
-    $contents = file_get_contents($template_name);
-  else {
+  //Get the path of the current file in pretty form for viewing
+  private function get_pretty_path() {
     $path = dirname($_SERVER['PHP_SELF']);
     if (substr($path,-1)=='\\') $path = substr($path,0,count($path)-1);
-    die( "<b>$path/$template_name could not be located</b><br/><br/>" );
-  }  
-  
-  include $template_name;
-/*
-  //Record all expected replacements
-  preg_match_all('/\$__(\w+)__/s',$contents,$matches);
-  foreach($matches[1] as $match) {
-    array_key_exists($match,$vars) ? $vars[$match]++ : $vars[$match]=1;
+    return $path;
   }
   
-  //Go through and do the replacements
-  //Subtract from var count each time its found
-  foreach($template_vars as $key => $value) {
-    $contents = str_replace('$__'.$key.'__',$value,$contents,$count);
-    if($count==0)
-      array_push($warnings,"$key is not a variable in this template");  
-    unset($vars[$key]); //Already set warning, remove variable
+  public function show_warnings() {
+    //If they want to see the warnings then show them here
+      echo "<b>Showing ".count($this->warnings)." Warnings:</br>".implode("<br/>\n",$this->warnings)."<br/></b>";
   }
   
-  //Make sure all template variables were given
-  foreach($vars as $key => $value)
-    array_push($warnings,"Template is missing $key variable value");
+  public function load_template( $label, $template ) {
   
-  preg_match_all('/%__(\w+)__func(.*?)%__end_func__/s');
-  for($i=0; $i<count($matches[1]); $i++) {
-    switch($matches[1][$i]) {
-      case 'load_template':
-        
-        break;
-      default:
-        array_push($warnings,"Template Engine does not support the ".$matches[2][$i]." function.");
-        break;
+    //Make sure they supplied a template name
+    if( !array_key_exists('name',$template) ) {
+      if ($this->dump_warnings) $this->show_warnings();
+      die( "<b>Template Name was not supplied</b><br/><br/>" );
     }
+    
+    //If the file exists, get the contents so we can check it first
+    if(file_exists($template['name']))
+      $contents = file_get_contents($template['name']);
+    else {
+      if ($this->dump_warnings) $this->show_warnings();
+      die( "<b>".$this->get_pretty_path()."/".$template['name']." could not be located for $label</b><br/><br/>" );
+    }
+   
+    //Find all the variable references and make sure those variables exist
+    preg_match_all('/\$template\[["\'](\w+)["\']\]/s',$contents,$matches);
+    foreach($matches[1] as $match) {
+      //If the a variable required by the template is missing
+      //Create an appropriate warning and push it on the stack
+      if( !array_key_exists($match,$template) )
+        array_push($this->warnings,"\"$match\" variable value was not supplied to $label");
+    } 
+      
+    preg_match('/<!\-\-REQUIRED_FILES(.*?)\-\->/s',$contents,$matches);
+    preg_match_all("/(stylesheet|javascript) ([\w\/\.\-]+)/s",$matches[1],$matches);
+    
+    for($i=0; $i<count($matches[0]); $i++) {
+      switch($matches[1][$i]) {
+        case 'javascript':$filename='js/'.$matches[2][$i].'.js';break;
+        case 'stylesheet':$filename='css/'.$matches[2][$i].'.css';break;
+      }
+      if( file_exists($filename) ) array_push( $this->req_files, $filename );
+      else array_push($this->warnings,"\"$filename\" required file cannot be found for $label");
+    }
+    
+    //Return the output from evaluating the template
+    ob_start();
+    eval("?".">".file_get_contents($template['name']));
+    $ret = ob_get_contents();
+    ob_clean();
+    return $ret;
+    //This throws the file contents into our page after we've checked everything out
+    //include $template['name'];
   }
   
+  public function add_link($file,$type) {
+    if( file_exists($file) ) {
+      if( $type == "js") 
+        array_push( $this->scripts, $file );
+      else if ($type == "css")
+        array_push( $this->styles, $file );
+    }
+    else
+      array_push($this->warnings,"\"$filename\" required file cannot be found");
+  }
   
+  public function linking_code() {
+    foreach($this->styles as $file)
+      echo "<link href=\"$file\" type=\"text/css\" rel=\"stylesheet\" />\n";
+    foreach($this->scripts as $file)
+      echo "<script src=\"$file\" type=\"text/javascript\"></script>\n";
+  }
   
-  
-  
-  
-  if($show_warnings && count($warnings)!=0)
-    die( "<b>".implode("<br/>\n",$warnings)."</br><br/></b>" );
-  
-  echo $contents;
-*/
-}
+};
 
+$templater = new Templater();
+$templater->warnings_on = true;
 ?>
