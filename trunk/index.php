95d2c0ff33e1515ccad268d7ce6165e1cb79b139
graylin.kim <graylin.kim@4cf41a06-4af6-11de-8001-7f2440edb24e>
2009-06-16 17:35:51 +0000

    Incorporated a neat little templating system based of a templater class and capturing eval output in output buffers.

--- trunk/index.php
+++ trunk/index.php
@@ -1,4 +1,32 @@
-<?php require_once "templates.php"; ?>
+<?php 
+  require_once "templates.php";
+  ($_GET['area']=="") ? $area = 'about' : $area = strtolower($_GET['area']);  
+  
+  $global_nav = $templater->load_template(  "global_nav",
+                              array(  'name' => 'global_navigation.tmpl',
+                                      'active_section' => $area,
+                                      'active_section_class' => 'navigation_area_active', 
+                                      'button_class' => 'navigation_area',
+                                      'buttons' => array('login'      =>'Login Area',
+                                                        'find'        =>'Find',
+                                                        'create'      =>'Create',
+                                                        'about'       =>'About',
+                                                        'contact'     =>'Contact',
+                                                        'developers'  =>'Developers',
+                                                        'legals'      =>'Legal Docs') ) ); 
+                                                        
+  $section_header = $templater->load_template(  "section_header",
+                              array(  'name' => 'section_header.tmpl',
+                                      'heading' => 'What is Flitter?',
+                                      'sublinks' => array( 'Sublink1' => '#',
+                                                           'Sublink2' => '#',
+                                                           'Sublink3' => '#' ),
+                                      'sublink_class' => 'section_nav_link') );
+                                      
+  $section_content = $templater->load_template(  "section_content",
+                              array( 'name' => $area.'.tmpl' ) );
+                                    
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" 
 "http://www.w3.org/TR/html4/strict.dtd">
 <html>
@@ -8,37 +36,27 @@
   <link href="css/index.css" type="text/css" rel="stylesheet" />
   <script src="js/jquery-1.3.2.js" type="text/javascript"></script>
   <script src="js/index.js" type="text/javascript"></script>
+  <?php $templater->linking_code(); ?>
 </head>
 
 <body>
   <div id="container">
-    <?php load_template("global_navigation.tmpl", array( 'site_logo' => 'Flitter', 
-                                                         'active_section' => 'About', 
-                                                         'button_class' => 'navigation_area',
-                                                         'buttons' => array('login'   =>'Login Area',
-                                                                            'find'    =>'Find',
-                                                                            'create'  =>'Create',
-                                                                            'about'   =>'About',
-                                                                            'contact' =>'Contact',
-                                                                            'devs'    =>'Developers',
-                                                                            'legals'  =>'Legal Docs') ) ); ?>
+  
+    <?php //Global Navigation Include
+      echo $global_nav;
+    ?>
+
     <div id="section_content">
-      
-      <div id="section_header">
-        <div id="section_logo">What is Flitter?</div>
-        <div id="section_navigation">
-          <a href="#" class="section_nav_link">
-            Sublink3
-          </a>
-          <a href="#" class="section_nav_link">
-            Sublink2
-          </a>
-          <a href="#" class="section_nav_link">
-            Sublink1
-          </a>
-        </div>
-      </div>
+    
+      <?php //Section Header Include
+        echo $section_header;
+      ?>
+      <?php //Section Content Include
+        echo $section_content;
+      ?>
     </div>
+    
   </div>
+  
 </body>
 </html>
